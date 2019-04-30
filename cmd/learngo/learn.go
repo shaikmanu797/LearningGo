46bfb7983b69385177338b148dac3dbc127c0020
Mansoor Baba Shaik <shaikmanu797@gmail.com>
2019-04-30 11:42:07 -0400

    Working with importing internal packages

--- cmd/learngo/learn.go
+++ cmd/learngo/learn.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/shaikmanu797/LearningGo/internal/app/learngo/utils"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println(utils.Reverse("!eyB"))
 }
